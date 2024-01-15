4095c8fdd603bf8b302c1c2216c7e126e0f9f323
Shubham Sharma <shubhsa@google.com>
2024-01-15 11:33:33 +0530

    Integrating with Materials UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import FilesTable from "./components/FilesTable.tsx";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import theme from "assets/theme";
 
 function App() {
-  return <div><FilesTable /></div>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <FilesTable />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const container = document.getElementById("root");
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );

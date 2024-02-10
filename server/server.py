39923a4c17d396ddfa6df9f267fb0af1196f6e74
byvirtue <akshay10275@gmail.com>
2024-02-10 21:54:02 +0530

    move to storj from s3

--- server/server.py
+++ server/server.py
@@ -11,7 +11,11 @@ from dao.nodeDAO import NodeHierarchy
 import boto3
 from dao.s3Accessor import S3Accessor
 from botocore.client import Config
+from dotenv import load_dotenv
 
+load_dotenv('./credentials.env')
+STORJ_ACCESS_KEY = os.environ.get('ACCESS_KEY')
+STORJ_SECRET_KEY = os.environ.get('SECRET_KEY')
 app = Flask("Memories")
 CORS(app, supports_credentials=True)
 
@@ -27,6 +31,7 @@ app.register_blueprint(file_bp)
 
 ##Move to config file
 def configure(binder):
+
     binder.bind(
         NodeHierarchy,
         to=NodeHierarchy(boto3.resource("dynamodb", region_name="ap-south-1")),
@@ -34,10 +39,9 @@ def configure(binder):
     )
     binder.bind(
         S3Accessor,
-        to=S3Accessor(boto3.client("s3", region_name="ap-south-1", config=Config(signature_version='s3v4'))),
+        to=S3Accessor(boto3.client("s3", aws_access_key_id=STORJ_ACCESS_KEY, aws_secret_access_key=STORJ_SECRET_KEY, endpoint_url = "https://gateway.storjshare.io", config=Config(signature_version='s3v4'))),
         scope=singleton,
     )
-
 
 FlaskInjector(app=app, modules=[configure])
 

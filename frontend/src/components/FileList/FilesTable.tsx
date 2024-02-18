31ae2e48cafd9fce7065e7a32b95183a805999b1
byvirtue <akshay10275@gmail.com>
2024-02-18 15:04:25 +0530

    integrate bulk upload with backend

--- frontend/src/components/FileList/FilesTable.tsx
+++ frontend/src/components/FileList/FilesTable.tsx
@@ -8,6 +8,7 @@ import MKBox from "components/MKBox/index.js";
 import Grid from "@mui/material/Grid";
 import Checkbox from "@mui/material/Checkbox";
 import { FileUpload } from "primereact/fileupload";
+import { ProgressBar } from 'primereact/progressbar';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { useRef } from "react";
@@ -24,12 +25,16 @@ function FilesTable() {
   const [selectAllChecked, setSelectAllChecked] = React.useState(false);
   const [folderMap, setFolderMap] = React.useState({"/": "root"});
 
+  const [filesUploaded, setFilesUploaded] = React.useState(0);
+  const [progress, setProgress] = React.useState(0);
+  const [uploading, setUploading] = React.useState(false);
+
   React.useEffect(() => {
     const folderId = folderMap[currentPath];
     fetchFileList(folderId)
       .then(response => setData(response))
       .catch(error => console.error('Error fetching file list:', error));
-  }, [currentPath, folderMap]);
+  }, [currentPath, folderMap, filesUploaded]);
 
   const fetchFileList = async (folderId) => {
     try {
@@ -61,6 +66,7 @@ function FilesTable() {
     }
     setSelectAllChecked(false);
   };
+
   const handleSelectAllChange = () => {
     setSelectAllChecked((prevSelectAllChecked) => !prevSelectAllChecked);
     setSelectedFiles((prevSelectedFiles) => {
@@ -174,118 +180,152 @@ function FilesTable() {
       </table>
     </MKBox>
   );
-}
 
-function CurrentPathElement(currentPath, handlePathClick, data, selectedFiles) {
-  const pathParts = currentPath.split("/").filter(Boolean);
-  const handleDownloadSelected = () => {
-    // Implement download logic for selected files
-    // Iterate through selectedFiles and initiate download for each file
-    selectedFiles.forEach((fileId) => {
-      const file = data.find((file) => file.id === fileId);
-      if (file && file.link && file.type !== "folder") {
-        const downloadLink = document.createElement("a");
-        downloadLink.href = file.link;
-        downloadLink.download = file.name;
-        console.log(downloadLink);
+
+  function CurrentPathElement(currentPath, handlePathClick, data, selectedFiles) {
+    const pathParts = currentPath.split("/").filter(Boolean);
+    const handleDownloadSelected = () => {
+      // Implement download logic for selected files
+      // Iterate through selectedFiles and initiate download for each file
+      selectedFiles.forEach((fileId) => {
+        const file = data.find((file) => file.id === fileId);
+        if (file && file.link && file.type !== "folder") {
+          const downloadLink = document.createElement("a");
+          downloadLink.href = file.link;
+          downloadLink.download = file.name;
+          console.log(downloadLink);
+        }
+      });
+    };
+
+    const handleDeleteSelected = () => {
+      // Implement download logic for selected files
+      // Iterate through selectedFiles and initiate download for each file
+      selectedFiles.forEach((fileId) => {
+        const file = data.find((file) => file.id === fileId);
+        if (file && file.link && file.type !== "folder") {
+          console.log(file);
+        }
+      });
+    };
+    const fileUploadRef = useRef(null);
+
+    const generateSignedUrl = async (name, folderId) => {
+      try {
+        const response = await axios.post(BACKEND_URL + "/initiate_upload", {
+          'parent_node_id': folderId,
+          'node_name': name,
+        });
+        console.log(response)
+        return await response.data;
+      } catch (error) {
+        console.error('Error generating signed URL:', error);
+        return null;
       }
-    });
-  };
-  const handleDeleteSelected = () => {
-    // Implement download logic for selected files
-    // Iterate through selectedFiles and initiate download for each file
-    selectedFiles.forEach((fileId) => {
-      const file = data.find((file) => file.id === fileId);
-      if (file && file.link && file.type !== "folder") {
-        console.log(file);
+    };
+
+    const confirmFileUpload = async (id) => {
+      try {
+        const response = await axios.post(BACKEND_URL + "/confirm_upload_status", {
+          'id': id,
+        });
+        console.log(response)
+        return await response.data;
+      } catch (error) {
+        console.error('Error confirming file status:', error);
+        return null;
       }
-    });
-  };
-  const fileUploadRef = useRef(null);
+    };
 
-  const customBase64Uploader = async (event) => {
-    console.log(event);
-    // convert file to base64 encoded
-    for (let i = 0; i < event.files.length; ++i) {
-      const file = event.files[i]; // Use 'i' to iterate through all files
+    const customBase64Uploader = async (event) => {
+      setUploading(true);
+      setProgress(0);
+      const folderId = folderMap[currentPath];
+      console.log(event);
+      console.log(folderId);
+      // convert file to base64 encoded
+      for (let i = 0; i < event.files.length; ++i) {
+        const file = event.files[i]; // Use 'i' to iterate through all files
 
-      // Read the file as a blob
-      let blob = await fetch(file.objectURL).then((r) => r.blob());
+        // generate signed url
+        const uploadData = await generateSignedUrl(file.name, folderId);
+        console.log(uploadData)
+        const uploadResponse = await axios.put(uploadData.upload_url, file, {
+          });
+        const fileStatus = await confirmFileUpload(uploadData.id);
+        setProgress(((i*100)/event.files.length));
+      }
 
-      // Create a FileReader instance
-      const reader = new FileReader();
+      setUploading(false);
+      setFilesUploaded(event.files.length);
+      setProgress(0);
+      if (fileUploadRef.current) {
+        fileUploadRef.current.clear();
+      }
+    };
 
-      // Read the blob as a data URL
-      reader.readAsDataURL(blob);
-
-      // Read the file data and compute the hash
-      reader.onloadend = function () {
-        const base64data = reader.result;        
-        console.log(`File ${i + 1} Hash: ${base64data}`); // Log the hash
-      };
-    }
-  };
-
-  return (
-    <>
-      <MKBox>
-        <Grid container>
-          <Grid item md={10} marginLeft={2}>
-            Current Path:
-            <span className="path-link" onClick={() => handlePathClick("/")}>
-            </span>
-            {pathParts.map((part, index) => (
-              <React.Fragment key={index}>
-                {" / "}
-                <span
-                  className="path-link"
-                  onClick={() =>
-                    handlePathClick(
-                      `/${pathParts.slice(0, index + 1).join("/")}`
-                    )
-                  }
-                >
-                  {part}
-                </span>
-              </React.Fragment>
-            ))}
+    return (
+      <>
+        <MKBox>
+          <Grid container>
+            <Grid item md={10} marginLeft={2}>
+              Current Path:
+              <span className="path-link" onClick={() => handlePathClick("/")}>
+              </span>
+              {pathParts.map((part, index) => (
+                <React.Fragment key={index}>
+                  {" / "}
+                  <span
+                    className="path-link"
+                    onClick={() =>
+                      handlePathClick(
+                        `/${pathParts.slice(0, index + 1).join("/")}`
+                      )
+                    }
+                  >
+                    {part}
+                  </span>
+                </React.Fragment>
+              ))}
+            </Grid>
+            <Grid item mr={2}>
+              <MKButton
+                variant="gradient"
+                color="info"
+                onClick={handleDeleteSelected}
+                size="small"
+              >
+                Delete Selected
+              </MKButton>
+            </Grid>
+            <Grid item>
+              <MKButton
+                variant="gradient"
+                color="info"
+                onClick={handleDownloadSelected}
+                size="small"
+              >
+                Download Selected
+              </MKButton>
+            </Grid>
           </Grid>
-          <Grid item mr={2}>
-            <MKButton
-              variant="gradient"
-              color="info"
-              onClick={handleDeleteSelected}
-              size="small"
-            >
-              Delete Selected
-            </MKButton>
-          </Grid>
-          <Grid item>
-            <MKButton
-              variant="gradient"
-              color="info"
-              onClick={handleDownloadSelected}
-              size="small"
-            >
-              Download Selected
-            </MKButton>
-          </Grid>
-        </Grid>
-        <br></br>
-      </MKBox>
-      <MKBox>
-        <FileUpload
-          ref={fileUploadRef}
-          name="Uploader"
-          accept="*"
-          maxFileSize={1000000}
-          customUpload
-          uploadHandler={customBase64Uploader}
-          multiple
-        />
-      </MKBox>
-    </>
-  );
+          <br></br>
+        </MKBox>
+        <MKBox>
+          <FileUpload
+            ref={fileUploadRef}
+            name="Uploader"
+            accept="*"
+            maxFileSize={1000000}
+            customUpload
+            uploadHandler={customBase64Uploader}
+            multiple
+          />
+          {uploading && <ProgressBar mode="determinate" value={progress} />}
+        </MKBox>
+      </>
+    );
+  }
 }
 
 export default FilesTable;

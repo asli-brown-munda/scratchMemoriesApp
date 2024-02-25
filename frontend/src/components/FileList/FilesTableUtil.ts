f237d4112edccbcb51643e940a116282a648c53e
byvirtue <akshay10275@gmail.com>
2024-02-25 16:33:00 +0530

    adds columns to table

--- frontend/src/components/FileList/FilesTableUtil.ts
+++ frontend/src/components/FileList/FilesTableUtil.ts
@@ -32,6 +32,19 @@ export const getExtension = function (filename) {
   }
 };
 
+export const getLocalTime = function (epochs) {
+  const epochNumber = parseInt(epochs);
+
+  // Create a Date object from the epoch (milliseconds since 1970-01-01)
+  const date = new Date(epochNumber);
+
+  // Use toLocaleString() to format the date based on the browser's locale
+  // and timezone, providing a user-friendly format
+  const formattedDate = date.toLocaleString();
+
+  return formattedDate.toString();
+};
+
 
 export const preProcessTableData = function (data) {
   var newArray = data.map((row) => {
@@ -41,8 +54,8 @@ export const preProcessTableData = function (data) {
     newRow["id"] = jsonRow['id'];
     newRow["name"] =  jsonRow['name'];
     newRow["type"] = jsonRow['type'];
-    newRow["size"] = niceBytes(2048);
-    newRow["update_at"] = jsonRow['created_at'];
+    newRow["size"] = jsonRow['type'] === 'folder' ? '' : niceBytes(jsonRow['metadata']['size']);
+    newRow["updated_at"] = getLocalTime(parseInt(jsonRow['created_at']));
     newRow["extension"] = getExtension(jsonRow['name'])
     return newRow;
   });

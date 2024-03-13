bd9747ac08a271faead0d954b03a62323c14b102
Shubham Sharma <s1.shubham@gmail.com>
2024-03-13 08:51:54 +0530

    Fixes dark mode issues

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'selector',
+  darkMode: 'false',
   content: [
     "./index.html",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
